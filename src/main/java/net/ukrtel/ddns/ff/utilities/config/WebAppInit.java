79758f2cedf452f61faa62757b6dc0b80ce63d55
spasynkov <s.pasynkov@gmail.com>
2017-12-08 15:14:26 +0200

    added OpenEntityManagerInViewFilter to fix problem with LazyInitializationException

--- src/main/java/net/ukrtel/ddns/ff/utilities/config/WebAppInit.java
+++ src/main/java/net/ukrtel/ddns/ff/utilities/config/WebAppInit.java
@@ -1,6 +1,9 @@
 package net.ukrtel.ddns.ff.utilities.config;
 
+import org.springframework.orm.jpa.support.OpenEntityManagerInViewFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.Filter;
 
 public class WebAppInit extends AbstractAnnotationConfigDispatcherServletInitializer {
     @Override
@@ -16,5 +19,10 @@ public class WebAppInit extends AbstractAnnotationConfigDispatcherServletInitial
     @Override
     protected String[] getServletMappings() {
         return new String[] {"/"};
+    }
+
+    @Override
+    protected Filter[] getServletFilters() {
+        return new Filter[]{new OpenEntityManagerInViewFilter()};
     }
 }
